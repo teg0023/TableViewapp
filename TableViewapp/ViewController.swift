b5926b45170b9dc8c04a3cefd621bf4c14f52273
teg0023 <teg0023@auburn.edu>
2024-02-19 09:48:39 -0600

    Initial Commit

---
+++ TableViewapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewapp
+//
+//  Created by user248010 on 2/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
